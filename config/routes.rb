5bfde029665bd272ca2870006e8b337205be25c2
shingo kubomura <skksb75@gmail.com>
2022-06-22 23:29:50 +0900

    add card style

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'restaurants#index'
   resources :restaurants, only: [ :index, :new, :create, :show ] do
     resources :reviews, only: [ :new, :create ]
   end
